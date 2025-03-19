de8b2abaaceecb36f0f9c20decb0f082ca364dff
superelastic <rfischer.public@gmail.com>
2025-03-19 17:42:23 +0000

    Save my local modifications to ollama demo

--- examples/lightrag_ollama_demo.py
+++ examples/lightrag_ollama_demo.py
@@ -4,7 +4,14 @@ from lightrag import LightRAG, QueryParam
 from lightrag.llm import ollama_model_complete, ollama_embedding
 from lightrag.utils import EmbeddingFunc
 
-WORKING_DIR = "./dickens"
+# neo4j
+BATCH_SIZE_NODES = 500
+BATCH_SIZE_EDGES = 100
+os.environ["NEO4J_URI"] = "bolt://localhost:7687"
+os.environ["NEO4J_USERNAME"] = "neo4j"
+os.environ["NEO4J_PASSWORD"] = "neo4j"
+
+WORKING_DIR = "/content/drive/MyDrive/LightRagProject/LightRAG//dickens"
 
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
@@ -14,7 +21,7 @@ if not os.path.exists(WORKING_DIR):
 rag = LightRAG(
     working_dir=WORKING_DIR,
     llm_model_func=ollama_model_complete,
-    llm_model_name="gemma2:2b",
+    llm_model_name="qwen2m",
     llm_model_max_async=4,
     llm_model_max_token_size=32768,
     llm_model_kwargs={"host": "http://localhost:11434", "options": {"num_ctx": 32768}},
@@ -25,9 +32,10 @@ rag = LightRAG(
             texts, embed_model="nomic-embed-text", host="http://localhost:11434"
         ),
     ),
+    graph_storage="Neo4JStorage",
 )
 
-with open("./book.txt", "r", encoding="utf-8") as f:
+with open("/content/drive/MyDrive/LightRagProject/data/book.txt", "r", encoding="utf-8") as f:
     rag.insert(f.read())
 
 # Perform naive search
